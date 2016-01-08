8ee6cbb8f7255229eacc109591e1442c1076eb8c
Ziliang Zeng <zzeng@umail.ucsb.edu>
2016-01-07 17:42:43 -0800

    added author and version number to MyFirstApp.java

--- MyFirstApp.java
+++ MyFirstApp.java
@@ -1,4 +1,11 @@
 public class MyFirstApp {
+
+    /** MyFirstApp is a sample program from p. 8-9 of Head First Java, 2nd edition.
+    @author Kathy Sierra adn Bert Bates
+    @author Ziliang Zeng
+    @version 01/07/2016
+*/
+    
     public static void main (String[] args){
 	System.out.println("I Rule!");
 	System.out.println("The World");
